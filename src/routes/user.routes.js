c3376d0c2061c764ead9bcb9362f13c585867f0a
SopanCristian <icsopanc@gmail.com>
2020-09-14 03:34:18 -0500

    [ADD] caching implement

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,10 +1,20 @@
 const { Router } = require("express");
-const { AuthMiddleware, ParseIntMiddleware } = require("../middlewares");
+const { 
+    AuthMiddleware, 
+    ParseIntMiddleware, 
+    CacheMiddleware,
+} = require("../middlewares");
+const { CACHE_TIME } = require("../helpers");
+const cacheMiddleware = require("../middlewares/cache.middleware");
 
 module.exports = function({ UserController }){
     const router = Router();
 
-    router.get("", [AuthMiddleware, ParseIntMiddleware], UserController.getAll);
+    router.get(
+        "", 
+        [AuthMiddleware, ParseIntMiddleware, cacheMiddleware(CACHE_TIME.ONE_HOUR)], 
+        UserController.getAll
+    );
     router.get("/:userId", UserController.get);
     router.patch("/:userId", UserController.update);
     router.delete("/:userId", UserController.delete);
